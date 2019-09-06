d196e11c4735bb6416e6a367923ed2c5dc4028d1
Carlos <carlosm@yzag.com>
2019-09-05 20:27:55 -0400

    refactorizing

--- app/serializers/article_group_serializer.rb
+++ app/serializers/article_group_serializer.rb
@@ -3,10 +3,12 @@ class ArticleGroupSerializer < ActiveModel::Serializer
   attributes :date, :article_list
 
   def date
-    object[0]
+    date = object.first
+    date
   end
 
   def article_list
-    object[1].map{ |item| ArticleSerializer.new(item) }
+    article_list = object.last.map{ |item| ArticleSerializer.new(item) }
+    article_list
   end
 end
