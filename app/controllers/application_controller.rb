9186aed728d0c3b66dbbbdf62ad9de4fa9c5e41e
carlos <carlosm880530@gmail.com>
2019-12-09 10:17:00 -0500

    enable GoogleDrive service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::API
 
   def initialize
     super
-    @drive_service = Services::GoogleDriveService.new if Rails.env.development?
+    @drive_service = Services::GoogleDriveService.new
   end
 end
